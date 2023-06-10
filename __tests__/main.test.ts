ebf3190698f22238d80b4cb881f9a7716ea39300
Bogdan Barbu <45259752+bogdanbeniaminb@users.noreply.github.com>
2023-06-10 17:01:01 +0300

    test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,11 +5,18 @@ import {expect, test} from '@jest/globals'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  // Create the "live-mirror" branch locally.
+  // This is necessary because the action will fail if the branch doesn't exist.
+  cp.execSync('git checkout -b live-mirror')
+  cp.execSync('git checkout main')
+
+  // Set the environment variables to run the action locally.
+  process.env['INPUT_RUN_LOCALLY_ONLY'] = 'true'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  const result = cp.execFileSync(np, [ip], options).toString()
+  console.log(result)
 })
